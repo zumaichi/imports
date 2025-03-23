6dfc9119fd04606072a6bde521622d3b31a80a24
zumaichi <dzumaquero@gmail.com>
2025-03-23 15:22:01 +0100

    model

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,11 @@
 import "./style.css";
-
-let puntuacion = 0;
-
-const calcularNumeroAleatorio = () => Math.floor(Math.random() * 10) + 1;
-
-const obtenerValorCarta = (numeroAleatorio: number) =>
-  numeroAleatorio > 7 ? 0.5 : numeroAleatorio;
-
-const obtenerValorUrlCarta = (numeroAleatorio: number) =>
-  numeroAleatorio > 7 ? numeroAleatorio + 2 : numeroAleatorio;
-
-const cartaUrl = (numeroAleatorio?: number) => {
-  switch (numeroAleatorio) {
-    case 1:
-      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg";
-
-    case 2:
-      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg";
-
-    case 3:
-      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg";
-
-    case 4:
-      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg";
-
-    case 5:
-      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg";
-
-    case 6:
-      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg";
-
-    case 7:
-      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
-
-    case 10:
-      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
-
-    case 11:
-      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
-
-    case 12:
-      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
-
-    default:
-      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
-  }
-};
+import {
+  calcularNumeroAleatorio,
+  obtenerValorCarta,
+  obtenerValorUrlCarta,
+  juego,
+  cartaUrl,
+} from "./model";
 
 const muestraCarta = (numeroAleatorio?: number) => {
   const imagen = document.getElementById("imagenCarta");
@@ -62,11 +22,11 @@ const muestraCarta = (numeroAleatorio?: number) => {
 };
 
 const aumentarPuntuacion = (valor: number) => {
-  puntuacion += valor;
+  juego.puntuacion += valor;
 
   const puntuacionElemento = document.getElementById("puntuacion");
   if (puntuacionElemento !== null) {
-    puntuacionElemento.innerHTML = `${puntuacion}`;
+    puntuacionElemento.innerHTML = `${juego.puntuacion}`;
   } else {
     console.error("No se ha encontrado el elemento de puntuación");
   }
@@ -161,16 +121,16 @@ const pedirCarta = () => {
   const valorUrlCarta = obtenerValorUrlCarta(numeroAleatorio);
 
   muestraCarta(valorUrlCarta);
-  console.log("puntuacion: ", puntuacion);
+  console.log("puntuacion: ", juego.puntuacion);
 
-  if (puntuacion >= 7.5) {
-    mostrarEstado(puntuacion);
+  if (juego.puntuacion >= 7.5) {
+    mostrarEstado(juego.puntuacion);
     deshabilitarBotones();
   }
 };
 const reinicio = () => {
-  puntuacion = 0;
-  document.getElementById("puntuacion")!.innerHTML = `${puntuacion}`;
+  juego.puntuacion = 0;
+  document.getElementById("puntuacion")!.innerHTML = `${juego.puntuacion}`;
   habilitarBotones();
   muestraCarta();
   desactivarSguir();
@@ -215,7 +175,7 @@ const cargarEventos = () => {
       activarReinicio();
       activarSguir();
       deshabilitarBotones();
-      mostrarEstado(puntuacion);
+      mostrarEstado(juego.puntuacion);
     });
   } else {
     console.error("error al plantarte");
@@ -231,7 +191,7 @@ const cargarEventos = () => {
     botonReinicio.addEventListener("click", () => {
       reinicio();
       anularReinicio();
-      mostrarEstado(puntuacion);
+      mostrarEstado(juego.puntuacion);
     });
   } else {
     console.error("Error al inicializar el botón de reinicio");
---
+++ src/model.ts
@@ -0,0 +1,62 @@
+import "./main.ts";
+
+interface juego {
+  puntuacion: number;
+  calcularNumeroAleatorio: number;
+  obtenerValorCarta: number;
+  obtenerValorUrlCarta: number;
+  cartaUrl: number;
+}
+
+export const juego: juego = {
+  puntuacion: 0,
+  calcularNumeroAleatorio: 0,
+  obtenerValorCarta: 0,
+  obtenerValorUrlCarta: 0,
+  cartaUrl: 0,
+};
+
+export const calcularNumeroAleatorio = () => Math.floor(Math.random() * 10) + 1;
+
+export const obtenerValorCarta = (numeroAleatorio: number) =>
+  numeroAleatorio > 7 ? 0.5 : numeroAleatorio;
+
+export const obtenerValorUrlCarta = (numeroAleatorio: number) =>
+  numeroAleatorio > 7 ? numeroAleatorio + 2 : numeroAleatorio;
+
+export const cartaUrl = (numeroAleatorio?: number) => {
+  switch (numeroAleatorio) {
+    case 1:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg";
+
+    case 2:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg";
+
+    case 3:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg";
+
+    case 4:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg";
+
+    case 5:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg";
+
+    case 6:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg";
+
+    case 7:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
+
+    case 10:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
+
+    case 11:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
+
+    case 12:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
+
+    default:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
+  }
+};
